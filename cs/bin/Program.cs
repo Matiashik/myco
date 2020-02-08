c168c5fd6ef83e0335c1ea7a285428e47fe740c1
Universai <solomatin377@gmail.com>
2020-02-08 19:21:56 +0300

    stable_3.1-ex

--- cs/bin/Program.cs
+++ cs/bin/Program.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
+using System.Collections.Generic;
 
 namespace bin
 {
@@ -7,16 +7,19 @@ namespace bin
     {
         static void Main(string[] args)
         {
-            var Con = new SqlConnection("Data Source=localhost;Initial Catalog=UVDB;User Id=universai;Password=solomatin11");
+            var Con = new SqlConnection("Data Source=localhost;Initial Catalog=UVDB;User Id=Univ;Password=Solomatin11");
             Con.Open();
             var Stmt = Con.CreateCommand();
-            Stmt.CommandText = "SELECT * FROM dbo.People";
+            //////////////////////////////////////////////////////
+            Stmt.CommandText = "SELECT * FROM phones where TECH = 'XIAOMI'";
             var Exq = Stmt.ExecuteReader();
+            var A = new List<int>();
             while(Exq.Read())
             {
-                Console.Write(Exq.GetInt32(0) + " ");
-                Console.WriteLine(Exq.GetString(1));
+                A.Add(Exq.GetInt32(1));
             }
+            System.Console.WriteLine(A.ToArray().Length);
+            //////////////////////////////////////////////////////
             Con.Close();
         }
     }
--- jvm/uvdb/src/com/universai/main/Main.java
+++ jvm/uvdb/src/com/universai/main/Main.java
@@ -1,22 +1,17 @@
 package com.universai.main;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         var con = DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=UVDB;user=universai;password=solomatin11");
         var stmt = con.createStatement();
-        var exq = stmt.executeQuery("SELECT * FROM dbo.People");
-        while (exq.next()) {
-            System.out.print(exq.getString("ID") + " ");
-            System.out.println(exq.getString("Name"));
+        var exq  = stmt.executeQuery("select * from phones where TECH = 'XIAOMI'");
+        var a = new ArrayList<Integer>();
+        while(exq.next()) {
+            a.add(exq.getInt(1));
         }
-        exq = stmt.executeQuery("INSERT INTO dbo.People VALUES (3, 'Matiashik')");
-        exq = stmt.executeQuery("SELECT * FROM dbo.People");
-        while (exq.next()) {
-            System.out.print(exq.getString("ID") + " ");
-            System.out.println(exq.getString("Name"));
-        }
-        exq = stmt.executeQuery()
+        System.out.println(a.size());
     }
 }
