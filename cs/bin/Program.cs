b79dd5a1cc698f582dc5d2225e4ace13495f2d96
Universai <solomatin377@gmail.com>
2020-02-10 19:12:34 +0300

    stable_3.1-ex

--- cs/bin/Program.cs
+++ cs/bin/Program.cs
@@ -1,26 +1,10 @@
-﻿using System.Data.SqlClient;
-using System.Collections.Generic;
-
-namespace bin
+﻿namespace bin
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var Con = new SqlConnection("Data Source=localhost;Initial Catalog=UVDB;User Id=Univ;Password=Solomatin11");
-            Con.Open();
-            var Stmt = Con.CreateCommand();
-            //////////////////////////////////////////////////////
-            Stmt.CommandText = "SELECT * FROM phones where TECH = 'XIAOMI'";
-            var Exq = Stmt.ExecuteReader();
-            var A = new List<int>();
-            while(Exq.Read())
-            {
-                A.Add(Exq.GetInt32(1));
-            }
-            System.Console.WriteLine(A.ToArray().Length);
-            //////////////////////////////////////////////////////
-            Con.Close();
+            System.Console.WriteLine("Hello world!");
         }
     }
 }
--- jvm/uvdb/src/com/universai/Main.java
+++ jvm/uvdb/src/com/universai/Main.java
@@ -1,17 +1,31 @@
-package com.universai.main;
+package com.universai;
 
-import java.sql.*;
+import com.mongodb.MongoClient;
+import org.bson.Document;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        var con = DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=UVDB;user=universai;password=solomatin11");
-        var stmt = con.createStatement();
-        var exq  = stmt.executeQuery("select * from phones where TECH = 'XIAOMI'");
-        var a = new ArrayList<Integer>();
-        while(exq.next()) {
-            a.add(exq.getInt(1));
+    public static void main(String[] args) {
+        var mgcl = new MongoClient("localhost");
+        var mgdb = mgcl.getDatabase("uvdb");
+        var col = mgdb.getCollection("List");
+        var lang = new String[] {"GO", "JAVA", "C#", "F#", "C++", "JS"};
+        var docs = new ArrayList<Document>();
+        for(int i = 0; i < 1000; i++) {
+            try {
+                docs.add(new Document("lang", lang[(int)(Math.random() * 10)])
+                        .append("progger", "Matiash")
+                        .append("i", i)
+                );
+            } catch (Exception ex) {
+                docs.add(new Document("lang", "Python")
+                        .append("progger", "Pidaras")
+                        .append("i", i)
+                );
+            }
         }
-        System.out.println(a.size());
+        col.insertMany(docs);
     }
 }
