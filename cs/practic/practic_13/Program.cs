3110f560c53adec37dc28696b7c52bc2f1902438
Universai <solomatin377@gmail.com>
2020-02-13 16:13:29 +0300

    stable_3.2

--- cs/practic/practic_13/Program.cs
+++ cs/practic/practic_13/Program.cs
@@ -6,10 +6,50 @@ namespace practic_13
     {
         static void Main(string[] args)
         {
-            var point = new Point3D(3, 2, 5);
-            point.Print();
-            point.Move('z', 3);
-            point.Print();
+            Point3D point1 = new Point3D(3, 2, 5);
+            Point3D point2 = new Point3D(5, 4, 1);
+            (point1 + point2).Print();
+        }
+    }
+    class Point3D
+    {
+        int x;
+        int y;
+        int z;
+        public Point3D() {x = y = z = 0;}
+        public Point3D(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public void Move(char b, int a)
+        {
+            switch(b) 
+            {
+                case 'x': x += a; break;
+                case 'y': y += a; break;
+                case 'z': z += a; break;
+                default: System.Console.WriteLine("Error"); break;
+            }
+        }
+        public void Print()
+        {
+            System.Console.WriteLine($"x: {x}, y: {y}, z: {z}");
+        }
+        public double RadVec()
+        {
+            return Math.Sqrt((double)(x*x + y*y + z*z));
+        }
+        public void Sum(Point3D a)
+        {
+            this.x += a.x;
+            this.y += a.y;
+            this.z += a.z;
+        }
+        public static Point3D operator +(Point3D a, Point3D b)
+        {
+            return new Point3D(a.x + b.x, a.y + b.y, a.z + b.z);
         }
     }
 }
