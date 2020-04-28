f1fea1f2659ece4cb9ea0159c631a8c76a648957
Matiash <48833814+Matiashik@users.noreply.github.com>
2020-04-28 09:53:00 +0300

    ready

--- cs/fone/fone_5.2/Program.cs
+++ cs/fone/fone_5.2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using static System.Math;
 //    1
 //0        2
@@ -21,6 +21,9 @@ namespace fone_5._2
                 if (pac.X == sgh.X && pac.Y == sgh.Y) { System.Console.WriteLine("Game Over"); break; }
                 if (Field[pac.Y, pac.X] == 2) Field[pac.Y, pac.X] = 0;
                 if (Field[pac.Y, pac.X] == 3) { Field[pac.Y, pac.X] = 0; pac.v = 2; }
+                bool t = true;
+                foreach (var i in Field) { if (i == 2) { t = false; break; } }
+                if (t) { System.Console.WriteLine("You win!"); break; }
                 gh1.Move();
                 if (pac.X == gh1.X && pac.Y == gh1.Y) { System.Console.WriteLine("Game Over"); break; }
                 if (pac.X == gh2.X && pac.Y == gh2.Y) { System.Console.WriteLine("Game Over"); break; }
@@ -39,21 +42,21 @@ namespace fone_5._2
         }
         public static int[,] Field =
         {
-            {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
-            {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
-            {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
-            {1,1,1,1,0,0,0,1,1,1,1,0,1,0,0},
-            {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},
-            {0,0,0,0,1,1,1,1,1,1,1,1,1,0,0},
-            {0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,1,1,1,1,0,0,0,0,0},
-            {0,0,0,0,0,0,1,2,0,1,1,1,1,1,0},
-            {0,0,0,1,0,0,1,0,0,1,0,0,0,0,0},
-            {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
-            {1,0,0,1,1,1,1,1,1,1,1,0,0,0,0},
-            {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-            {1,1,1,1,0,1,1,1,0,0,1,1,1,0,1}
+            {2,2,2,1,2,2,2,2,2,2,2,2,2,2,2},
+            {2,2,2,1,2,2,2,2,2,2,2,2,2,2,2},
+            {2,2,2,1,2,2,2,2,2,2,2,2,2,2,2},
+            {1,1,1,1,2,2,2,1,1,1,1,2,1,2,2},
+            {2,2,2,2,2,2,2,2,2,2,2,2,1,2,2},
+            {2,2,2,2,1,1,1,1,1,1,1,1,1,2,2},
+            {2,2,2,2,2,2,1,2,2,2,2,2,2,2,2},
+            {2,2,2,2,2,2,1,2,2,2,2,2,2,2,2},
+            {2,2,2,2,2,2,1,1,1,1,2,2,2,2,2},
+            {2,2,2,2,2,2,1,2,2,1,1,1,1,1,2},
+            {2,2,2,1,2,2,1,2,2,1,2,2,2,2,2},
+            {2,2,2,1,2,2,2,2,2,2,2,2,2,2,2},
+            {1,2,2,1,1,1,1,1,1,1,1,2,2,2,2},
+            {1,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+            {1,1,1,1,2,1,1,1,2,2,1,1,1,2,1}
         };
         public static Pacman pac = new Pacman(7, 7, 0);
         public static Ghost gh1 = new Ghost(5, 3, 2);
@@ -72,7 +75,7 @@ namespace fone_5._2
                     else if (j == sgh.X && sgh.Y == i) sgh.Draw();
                     else
                     {
-                        Console.ForegroundColor = a[i, j] == 1 ? ConsoleColor.White : (a[i,j] == 2 ? ConsoleColor.DarkMagenta : ConsoleColor.Black);
+                        Console.ForegroundColor = a[i, j] == 1 ? ConsoleColor.White : (a[i, j] == 2 ? ConsoleColor.DarkMagenta : ConsoleColor.Black);
                         System.Console.Write(a[i, j]);
                         Console.ResetColor();
                     }
@@ -153,22 +156,22 @@ namespace fone_5._2
                     case "w":
                         if (Y > 0) { if (Program.Field[Y - v, X] != 1) { Y -= v; return; } }
                         else if (Program.Field[14, X] != 1) { Y = 14; return; }
-                        N = N < 4 ? (N + rand.Next())%4 : 0;
+                        N = N < 4 ? (N + rand.Next()) % 4 : 0;
                         break;
                     case "s":
                         if (Y < 14) { if (Program.Field[Y + v, X] != v) { Y += v; return; } }
                         else if (Program.Field[0, X] != 1) { Y = 0; return; }
-                        N = N < 4 ? (N + rand.Next())%4 : 0;
+                        N = N < 4 ? (N + rand.Next()) % 4 : 0;
                         break;
                     case "a":
                         if (X > 0) { if (Program.Field[Y, X - v] != v) { X -= v; return; } }
                         else if (Program.Field[Y, 14] != 1) { X = 14; return; }
-                        N = N < 4 ? (N + rand.Next())%4 : 0;
+                        N = N < 4 ? (N + rand.Next()) % 4 : 0;
                         break;
                     case "d":
                         if (X < 14) { if (Program.Field[Y, X + v] != v) { X += v; return; } }
                         else if (Program.Field[Y, 0] != 1) { X = 0; return; }
-                        N = N < 4 ? (N + rand.Next())%4 : 0;
+                        N = N < 4 ? (N + rand.Next()) % 4 : 0;
                         break;
                 }
             }
@@ -191,9 +194,42 @@ namespace fone_5._2
         public void Move(Pacman pac)
         {
             var a = N == 1 ? 'w' : (N == 0 ? 'a' : (N == 3 ? 'd' : 's'));
+            if (X == pac.X)
+            {
+                if (pac.Y < Y) a = 'w';
+                if (pac.Y > Y) a = 's';
+            }
+            if (Y == pac.Y)
+            {
+                if (pac.X < X) a = 'a';
+                if (pac.X > X) a = 'd';
+            }
             while (true)
             {
-                break;
+                switch (a.ToString().ToLower())
+                {
+                    case "w":
+                        if (Y > 0) { if (Program.Field[Y - v, X] != 1) { Y -= v; return; } }
+                        else if (Program.Field[14, X] != 1) { Y = 14; return; }
+                        N = N < 4 ? (N + rand.Next()) % 4 : 0;
+                        break;
+                    case "s":
+                        if (Y < 14) { if (Program.Field[Y + v, X] != v) { Y += v; return; } }
+                        else if (Program.Field[0, X] != 1) { Y = 0; return; }
+                        N = N < 4 ? (N + rand.Next()) % 4 : 0;
+                        break;
+                    case "a":
+                        if (X > 0) { if (Program.Field[Y, X - v] != v) { X -= v; return; } }
+                        else if (Program.Field[Y, 14] != 1) { X = 14; return; }
+                        N = N < 4 ? (N + rand.Next()) % 4 : 0;
+                        break;
+                    case "d":
+                        if (X < 14) { if (Program.Field[Y, X + v] != v) { X += v; return; } }
+                        else if (Program.Field[Y, 0] != 1) { X = 0; return; }
+                        N = N < 4 ? (N + rand.Next()) % 4 : 0;
+                        break;
+                }
+                a = N == 1 ? 'w' : (N == 0 ? 'a' : (N == 3 ? 'd' : 's'));
             }
         }
     }
